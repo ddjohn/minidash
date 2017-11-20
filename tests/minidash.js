9f8bd229e668f3186f88bfc02303bffcef345e32
David Johansson <ddjohn@gmail.com>
2017-11-20 21:38:38 +0100

    Simple post script

---
+++ tests/minidash.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var request = require('request');
+
+if(process.argv.length !== 6) {
+	console.log('');
+	console.log('syntax: ' + process.argv[0] + ' <icon> <name> <value> <status>');
+	console.log('');
+	process.exit(1);	
+}
+
+request.post('http://localhost:3000//measure/' + process.argv[2] + '/' + process.argv[3] + '/' + process.argv[4] + '/' + process.argv[5], 'message', function (error, response, body) {
+	console.log(body);
+});
